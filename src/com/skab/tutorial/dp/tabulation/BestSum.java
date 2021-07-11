d8a325577c4e94c0db26064181f8ba6854ecae14
Shamsul Kamal <shamsulkamal.abubakar@gmail.com>
2021-07-11 11:50:55 +0800

    add BestSum using tabulation

---
+++ src/com/skab/tutorial/dp/tabulation/BestSum.java
@@ -0,0 +1,37 @@
+package com.skab.tutorial.dp.tabulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BestSum {
+    public static void main(String[] args) {
+        System.out.println(bestSum(7, new int[] {5, 3, 4, 7})); // [7]
+        System.out.println(bestSum(8, new int[] {2, 3, 5}));    // [3, 5]
+        System.out.println(bestSum(8, new int[] {1, 4, 5}));    // [4, 4]
+        System.out.println(bestSum(100, new int[] {1, 2, 5, 25}));  // [25, 25, 25, 25]
+    }
+
+    private static List<Integer> bestSum(int targetSum, int[] numbers) {
+        List<List<Integer>> table = new ArrayList<>();
+        for (int i = 0; i < (targetSum + 1); i++) {
+            table.add(null);
+        }
+        table.set(0, new ArrayList<Integer>());
+
+        for (int i = 0; i <= targetSum; i++) {
+            if (table.get(i) != null) {
+                for (int num : numbers) {
+                    List<Integer> combination = new ArrayList<>(table.get(i));
+                    combination.add(num);
+                    try {
+                        if (table.get(i + num) == null || (table.get(i + num).size() > combination.size())) {
+                            table.set(i + num, combination);
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+        return table.get(targetSum);
+    }
+}
